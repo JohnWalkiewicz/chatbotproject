16c913884e96fdc3dfba13586f6f2bb48128c5df
John Walkiewicz <walkiewiczbrenda@gmail.com>
2016-09-22 10:36:03 -0700

    Added questions
    Added about 5 questions and responses to the chat bot

--- chatbot.js
+++ chatbot.js
@@ -6,18 +6,38 @@ function submitButton() {
    input =  document.getElementById("input").value;
    question = input + "<br>";
 
+//*Variables for all the Questions/responses*//
+
+   var n = input.search(/how are you/i);
+
 
    chatBox = document.getElementById("chat-area").innerHTML +=  "Question: " + question;
 
    document.getElementById("input").value = null;
 
-   if(input == "Hey dad"){
-     document.getElementById("chat-area").innerHTML +=  "Response: " + "Hello there son" + "<br>";
+   if(input == "hello" || input =="Hello"){
+     document.getElementById("chat-area").innerHTML +=  "Response: " + "Hello there! nice to meet you" + "<br>";
    }
+   /*how are you response*/
+
+   /*else if(n == -1){
+     document.getElementById("chat-area").innerHTML +=  "Response: " + "Im fine, how are you?" + "<br>";
+   }
+   else if(n > -1){
+     document.getElementById("chat-area").innerHTML +=  "Response: Im fine, how are  you?" + "" + "<br>";
+   }
+  /*How old are you question*/
+
    else if(input == "how old are you?"){
      document.getElementById("chat-area").innerHTML +=  "Response: " + "I am a robot, I have no age" + "<br>";
    }
-   else if(input == "what is your name?"){
+
+   /*What is your name question*/
+   else if(input == "what is your name?" && "What is your name?"){
     document.getElementById("chat-area").innerHTML +=  "Response:    You can call me H A R A M B E" + "<br>";
+   }
+
+   else{
+    document.getElementById("chat-area").innerHTML +=  "Response:  I dont think I got that, please try again." + "<br>";
    }
 }
