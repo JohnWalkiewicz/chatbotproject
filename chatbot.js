620c9c02c6bfbfde4e0f2df5f0579e8e7990aa38
John Walkiewicz <walkiewiczbrenda@gmail.com>
2016-09-29 09:03:21 -0700

    Added more atuff
    Added 2 more questions & more random responses!

--- chatbot.js
+++ chatbot.js
@@ -13,6 +13,10 @@ function submitButton() {
      return Math.floor((Math.random() * helloResponse.length));
    }
 
+   function randomNumberGeneratorForFallback(max,min) {
+     return Math.floor((Math.random() * fallBackResponse.length));
+   }
+
 /*Enter key*/
 
 
@@ -33,11 +37,30 @@ function submitButton() {
    var d = input.search(/hey thats pretty good/i);
    var e = input.search(/show me a picture/i);
    var f = input.search(/what is your favorite song?/i);
+   var j = input.search(/what is the meaning of life?/i);
+   var k = input.search(/what are you?/);
+  /*Keywords*/
+   var g = input.search(/sad/i);
+   var h = input.search(/good/i);
+   var i = input.search(/fine/i);
 
    /*Random Responses*/
   var helloResponse = ["Hello there!", "Hi!", "Hola!", "Hello welcome to chat bot"];
-   var nResponse = ["Im fine, how are you!", "Im doing swell, you?", "Im just a robot"];
-   var aResponse =
+   var nResponse = ["Im fine, how are you!", "Im doing swell, you?", "Im just a robot", "I have no feelings, you?"];
+   var aResponse = ["I am what ever age you want", "I am a robot I have no age", "Age doesnt matter, how old are you?", "Im 25, you?"];
+   var bResponse = ["My name is chatbot, what is yours?", "Me llamo es chatbot, y tu?", "Names don't matter", "Any name you prefer!"];
+   var fResponse = ["I like many different songs, you?", "Im a fan of the pop genre, what about you?", "Im into rap music,  what about  you?", "Im not really a fan of music."];
+   var jResponse = ["Whatever you want it to be.", "Everything", "Harambe could tell you, but he's not here anymore" ,"Be happy and help others be happy!:)"];
+
+  /*Random responses for keywords*/
+
+   var gResponse = ["Im sorry", "Why are you sad?", "I hope I can make you feel better", "Don't be sad"];
+   var hResponse = ["Im glad", "what makes you happy?", "Great!", "Im also good!"];
+   var iResponse = ["Glad to hear", "Im doing swell also", "Why?" , "Good thing your not sad"];
+
+   /*FALLBACK ANSWERS*/
+   var fallBackResponse = ["Cool!", "Why?" , "I dont understand", "Hey thats pretty good!", "Im sorry", "What?", "Please ask again" , "Wow!"]
+
 
 
 
@@ -52,16 +75,16 @@ function submitButton() {
 
    else if(n > -1){
 
-     document.getElementById("chat-area").innerHTML +=  "ChatBot: " + nResponse[randomNumberGenerator(0,2)] + "<br>";
+     document.getElementById("chat-area").innerHTML +=  "ChatBot: " + nResponse[randomNumberGenerator(0,3)] + "<br>";
    }
   /*How old areyou question*/
    else if(a > -1){
-     document.getElementById("chat-area").innerHTML +=  "ChatBot: " + "I am a robot, I have no age" + "<br>";
+     document.getElementById("chat-area").innerHTML +=  "ChatBot: " + aResponse[randomNumberGenerator(0,3)] + "<br>";
    }
 
    /*What is your name question*/
    else if(b > -1){
-    document.getElementById("chat-area").innerHTML +=  "ChatBot:    You can call me H A R A M B E" + "<br>";
+    document.getElementById("chat-area").innerHTML +=  "ChatBot: " + bResponse[randomNumberGenerator(0,3)]  + "<br>";
   }
 
   /*What is the time question*/
@@ -90,15 +113,33 @@ else if(e > -1){
 }
 /*whats your favorite song*/
 else if(f > -1){
- document.getElementById("chat-area").innerHTML += "ChatBot:    I like many different songs!" + "<br>";
- audio2.play();
+ document.getElementById("chat-area").innerHTML += "ChatBot:" + fResponse[randomNumberGenerator(0,3)] + "<br>";
+
+}
+/*what is the meaning of life?*/
+else if(j > -1){
+ document.getElementById("chat-area").innerHTML += "ChatBot:" + jResponse[randomNumberGenerator(0,3)] + "<br>";
+
+}
+/*Triggering responses to different keywords*/
+
+else if(g > -1){
+ document.getElementById("chat-area").innerHTML += "ChatBot:" + gResponse[randomNumberGenerator(0,3)] + "<br>";
+
 }
 
+else if(h > -1){
+ document.getElementById("chat-area").innerHTML += "ChatBot:" + hResponse[randomNumberGenerator(0,3)] + "<br>";
 
+}
 
+else if(i > -1){
+ document.getElementById("chat-area").innerHTML += "ChatBot:" + iResponse[randomNumberGenerator(0,3)] + "<br>";
 
-   else{
-    document.getElementById("chat-area").innerHTML +=  "ChatBot:    Thats cool!" + "<br>"
+}
+
+else{
+    document.getElementById("chat-area").innerHTML +=  "ChatBot:" + fallBackResponse[randomNumberGeneratorForFallback(0,7)] + "<br>"
    }
    document.getElementById("input").value = null;
 }
